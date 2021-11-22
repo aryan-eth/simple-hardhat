07de394913cb64f1a22672aad20a2c828a344517
Aryan Singh <aryansingh@Aryans-MacBook-Pro.local>
2021-11-21 17:11:48 -0800

    first

---
+++ contracts/SomethingNew.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract SomethingNew {
+
+    constructor(){
+        // console.log("Something");
+    }
+
+    struct Abc{
+        uint age;
+    }
+
+    function _something(string memory _str) private view returns (uint) {
+
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,10 @@
+const hre = require("hardhat");
+
+async function main(){
+
+    // compile
+    // deploy - we get an address when we deploy.
+
+    // await hre.ethers.getContractFactory("Greeter");
+
+}
